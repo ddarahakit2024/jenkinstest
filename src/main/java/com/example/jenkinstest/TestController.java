977da63ec06c0deec2d9531924e8f06fadc86028
ddarahakit <ddarahakit@gmail.com>
2024-08-23 10:10:50 +0900

    first commit

---
+++ src/main/java/com/example/jenkinstest/TestController.java
@@ -0,0 +1,17 @@
+package com.example.jenkinstest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping(method = RequestMethod.GET, value = "/test")
+    public ResponseEntity<String> test(String name, Long price) {
+
+        return ResponseEntity.ok("v1");
+    }
+}
